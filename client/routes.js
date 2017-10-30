c4da937610abe5525b2facb4c76a066d6a6225d5
cainiao152 <cainiao152@ufl.edu>
2017-10-30 18:13:09 -0400

    Add files via upload

---
+++ client/routes.js
@@ -0,0 +1,19 @@
+Router.route('/', {
+	name: 'homePage',
+	template: 'homePage'
+});
+
+Router.route('/signUp', {
+	name: 'signUpPage',
+	template: 'signUpPage'
+});
+
+Router.route('/signIn', {
+	name: 'signInPage',
+	template: 'signInPage'
+});
+
+Router.route('/event', {
+	name: 'event',
+	template: 'event'
+});
---
+++ imports/ui/pages/LoginPages/signUpPage.js
@@ -0,0 +1,1 @@
+import './signUpPage.html';
